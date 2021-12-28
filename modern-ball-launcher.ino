829c0151309e83d76011dbcbb48220b539870e6c
Charlie Steenhagen <csteen1005@gmail.com>
2021-12-28 10:08:25 -0600

    Updated to work with Brushed ESC

--- modern-ball-launcher.ino
+++ modern-ball-launcher.ino
@@ -10,8 +10,8 @@
 
 #include <Servo.h>
 #define MAX_VALUE 180
-#define MIN_VALUE 0
-#define MIN_PULSE_LENGTH 0 // Minimum pulse length in µs
+#define MIN_VALUE 90
+#define MIN_PULSE_LENGTH 90 // Minimum pulse length in µs
 #define MAX_PULSE_LENGTH 180 // Maximum pulse length in µs
 #define POT_PIN A2
 #define MOTOR_1_PIN 9
@@ -23,17 +23,20 @@ Servo ESC_2;
 int potValue;  // value from the analog pin
 
 void setup() {
+  pinMode(POT_PIN, INPUT);
+  
   delay(2000);
   
   // Attach the ESC on pin 9 & 10
   ESC.attach(MOTOR_1_PIN,1000,2000); // (pin, min pulse width, max pulse width in microseconds) 
   ESC_2.attach(MOTOR_2_PIN, 1000, 2000);
-  ESC.write(MIN_PULSE_LENGTH);
-  ESC_2.write(MIN_PULSE_LENGTH);
-  delay(1000);
+
+  //Calibrate The ESC's by sending Max value then Min value
   ESC.write(MAX_PULSE_LENGTH);
   ESC_2.write(MAX_PULSE_LENGTH);
-
+  delay(1000);
+  ESC.write(MIN_PULSE_LENGTH);
+  ESC_2.write(MIN_PULSE_LENGTH);
 }
 
 void loop() {
@@ -43,7 +46,7 @@ void loop() {
   potValue = map(potValue, 0, 1023, MIN_VALUE, MAX_VALUE);
 
   // Adds a buffer so the motors aren't trying to run at low values.
-  if(potValue < 15) potValue = 0;
+  if(potValue < 95) potValue = MIN_VALUE;
   
   ESC.write(potValue);
   ESC_2.write(potValue);
